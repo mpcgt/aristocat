1905fe9c3a4e77eb01d1f3f03d6f497fdd624c61
Aristocat <max.pcgt@gmail.com>
2023-10-14 12:48:32 +0000

    Modifications des textes de SQL

--- public/table_members.sql
+++ public/table_members.sql
@@ -1,6 +1,17 @@
-CREATE TABLE membre (
-	id int(11) NOT NULL auto_increment,
-	login text NOT NULL,
-	pass_md5 text NOT NULL,
-	PRIMARY KEY  (id)
-) TYPE=MyISAM;
+CREATE DATABASE mydb;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+ALTER TABLE posts
+ADD user_id INT,
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+INSERT INTO users (username, email)
+VALUES ('JohnDoe', 'john.doe@example.com');
+SELECT * FROM users;
+UPDATE users SET email = 'new_email@example.com'
+WHERE id = 1;
+DELETE FROM users
+WHERE id = 1;
